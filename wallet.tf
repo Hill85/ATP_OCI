920dd815757c5500709708ceb1135be930535262
Hilton Cousseau <hillgdi@gmail.com>
2021-05-01 15:27:58 -0300

    implementa wallet5

--- wallet.tf
+++ wallet.tf
@@ -9,5 +9,5 @@ resource "oci_database_autonomous_database_wallet" "autonomous_database_wallet" 
 
 resource "local_file" "autonomous_database_wallet_file" {
   content_base64 = oci_database_autonomous_database_wallet.autonomous_database_wallet.content
-  filename       = "./autonomous_database_wallet.zip" 
+  filename       = "/pass_wallet/autonomous_database_wallet.zip" 
 } 
