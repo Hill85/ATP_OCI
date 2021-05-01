e4fa9022c62f1d3715e4f264178473bdc85cdd8f
Hilton Cousseau <hillgdi@gmail.com>
2021-05-01 15:05:23 -0300

    impementa infra

---
+++ wallet.tf
@@ -0,0 +1,13 @@
+
+
+resource "oci_database_autonomous_database_wallet" "autonomous_database_wallet" {
+  autonomous_database_id = oci_database_autonomous_database.autonomous_database.id
+  password               = var.wallet
+  base64_encode_content  = "true"
+}
+
+
+resource "local_file" "autonomous_database_wallet_file" {
+  content_base64 = oci_database_autonomous_database_wallet.autonomous_database_wallet.content
+  filename       = "/Projetos_thebootcamp/finish_project_inovation/azure/app_files/wallet.zip" 
+} 
