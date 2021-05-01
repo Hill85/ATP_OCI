deed658a56640fa59d2c8f785bd49618672d017b
Hilton Cousseau <hillgdi@gmail.com>
2021-05-01 11:41:13 -0300

    Implementando a infra2

--- main.tf
+++ main.tf
@@ -47,6 +47,18 @@ variable "autonomous_database_is_dedicated" {
 
 }
 
+resource "oci_database_autonomous_database_wallet" "autonomous_database_wallet" {
+  autonomous_database_id = oci_database_autonomous_database.autonomous_database.id
+  password               = "Welcome123456"
+  base64_encode_content  = "true"
+}
+
+
+resource "local_file" "autonomous_database_wallet_file" {
+  content_base64 = oci_database_autonomous_database_wallet.autonomous_database_wallet.content
+  filename       = "autonomous_database_wallet.zip"
+}
+
 
 resource "oci_database_autonomous_database" "autonomous_database" {  
   admin_password           = var.admin_password
