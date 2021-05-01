bac6d36fb301f8697c9f0410d49e90341994d128
Hilton Cousseau <hillgdi@gmail.com>
2021-05-01 15:15:06 -0300

    implementa wallet2

--- wallet.tf
+++ wallet.tf
@@ -9,5 +9,5 @@ resource "oci_database_autonomous_database_wallet" "autonomous_database_wallet" 
 
 resource "local_file" "autonomous_database_wallet_file" {
   content_base64 = oci_database_autonomous_database_wallet.autonomous_database_wallet.content
-  filename       = "/Projetos_thebootcamp/finish_project_inovation/azure/app_files/wallet.zip" 
+  filename       = "/wallet.zip" 
 } 
