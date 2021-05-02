71e1602574535169ab1d959e5a154a105fc59b9b
Hilton Cousseau <hillgdi@gmail.com>
2021-05-01 21:45:07 -0300

    destroy infra2

--- wallet.tf
+++ wallet.tf
@@ -1,3 +1,5 @@
+/*
+
 resource "oci_database_autonomous_database_wallet" "autonomous_database_wallet" {
   autonomous_database_id = oci_database_autonomous_database.autonomous_database.id
   password               = "Welcome123456"
@@ -7,3 +9,5 @@ resource "local_file" "autonomous_database_wallet_file" {
   content_base64 = oci_database_autonomous_database_wallet.autonomous_database_wallet.content
   filename       = "autonomous_database_wallet.zip"
 }
+
+*/
